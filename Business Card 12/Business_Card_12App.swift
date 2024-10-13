adb18e6d68203382915c0d4ae1ef166edf48aa22
sahar <sahardaod4@gmail.com>
2024-10-13 22:03:48 +0800

    Initial Commit

---
+++ Business Card 12/Business_Card_12App.swift
@@ -0,0 +1,17 @@
+//
+//  Business_Card_12App.swift
+//  Business Card 12
+//
+//  Created by sahar daod on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Business_Card_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Business Card 12/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Business Card 12
+//
+//  Created by sahar daod on 10/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
